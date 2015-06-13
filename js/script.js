fa198822e988991635945bc0937ac77b469f0b1c
ushchent <ushchent@ushchent-lt>
2015-06-13 21:47:43 +0300

    Добавлена таблица с видами деятельности.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var tr;
+var selected;
+var groupsSelection, tbody;
+var groups;
 var width = 800,
     height = 600;
 var svg_map = d3.select("#map").append("svg").attr({width: width, height: height});
@@ -6,23 +10,31 @@ var projection = d3.geo.mercator().center([27.9, 53.7]).scale(4000)
 var path = d3.geo.path().projection(projection);
 var color = d3.scale.quantize()
               .range(['rgb(255,255,178)','rgb(254,204,92)','rgb(253,141,60)','rgb(227,26,28)']);
-var formatter = d3.format(",.1f")
-
-// Минимум - d3.max(color.invertExtent("rgb(254,204,92)"), function(d) { return Math.round(d); });
-
-//var calenderScale = d3.time.scale().domain([new Date("2015-01-01"), new Date("2015-04-30")])
-                      //.rangeRound([20, width - 20])
-                      //.nice(d3.time.month,)
-                      //.clamp(true);
+var formatter = d3.format(",.1f"),
+    formatter2 = d3.format(",.f");
 
 var calenderScale = d3.scale.ordinal()
                       .domain(["Январь", "Февраль", "Март", "Апрель"])
                       .rangePoints([30, width - 30]);
+            
+var months = d3.scale.ordinal()
+                      .domain(["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+                        "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"])
+                      .rangeRoundBands([11]);
+function setTableMonth(d) {
+    var tableMonths = ["январе", "феврале", "марте", "апреле", "мае", "июне", "июле", "августе", "сентябре", "октябре", "ноябре", "декабре"];
+    d3.select("#tablemonth").text(tableMonths[d]);
+}
                       
 var month = calenderScale.domain().length - 1;
 
+function appendMonth(d) {
+  d3.select("#month").text(months(d));
+}
+
+appendMonth(month);
+
 var sliderAxis = d3.svg.axis().scale(calenderScale).orient("bottom")
-                    //.tickValues(calenderScale.domain())
                     .ticks(3);
 
 d3.select("#scale").append("svg")
@@ -30,9 +42,6 @@ d3.select("#scale").append("svg")
       .append("g").attr("class", "x axis")
       .attr("transform", "translate(0, 3)")
       .call(sliderAxis);
-// 'Январь', 'Февраль', 'Март', 'Апрель'
-
-
 
 var stepper = width / (calenderScale.domain().length - 1);
 
@@ -43,13 +52,114 @@ function selectData(datum) {
   }
 }
 
+function selectSvodka(datum) {
+  if ((datum.rajon == "Всего") || (datum.rajon == "Минск")) {
+    return datum;
+  }
+}
+
+function convertTitleCase(title) {
+    var firstChar = title
+}
+
+setTableMonth(month);
+
 var slider = d3.slider().min(0).max(width).step(width / 3);
 d3.select("#slider").call(slider.value(width));
 
 d3.json("data/rajony.geojson", function(karta) {
-  d3.json("data/data.json", function(data) {
+  d3.csv("data/data.csv", function(data) {
     d3.csv("data/goroda.csv", function(goroda) {
-    var selection = data.filter(selectData);
+    
+    
+    d3.csv("data/groups.csv", function(groups) {
+
+    // Таблица
+        groups = groups;
+        
+        tableSelection = groups.filter(function(d) { if ((d.period == month) && (d.subgroup != "Всего")) { return d; }; } );
+        tableSelected = tableSelection.sort(function(a, b) { return d3.descending(parseInt(a['amount']), parseInt(b['amount'])); }).slice(0, 10);
+        
+        
+        var table = d3.select("#table");
+        var thead = table.append("thead").style("background-color", 'rgb(254,204,92)').style("color", "black");
+        tbody = table.append("tbody");
+        
+        thead.append("tr").selectAll("th").data(["Вид", "Зарплата, руб."]).enter().append("td").text(function(d) { return d; });
+        
+        tr = tbody.selectAll("tr").data(tableSelected)
+    
+        tr.enter().append("tr").style('background-color', function (d, i) { return i%2 ? 'white' : 'rgb(255,255,178)'; });;
+
+        
+        var tds = tr.selectAll("td");
+	tds.data(function(d) { var alist = []; alist.push(d.subgroup.toUpperCase(), formatter2(d.amount)); return alist }).enter().append("td").text(function(d) { return d; });
+
+
+	var selection = data.filter(selectData).sort(function(a, b) {
+	return d3.ascending(a.amount, b.amount)});
+	selected = selection;
+   
+    var svodka = d3.select("#svodka").append("svg").attr({width: width, height: 340});
+    var svodkaSelection = selection.filter(selectSvodka)
+    
+    
+    var min = selection[0], max = selection[selection.length - 1];
+    max.max = "max";
+    min.min = "min";
+    svodkaSelection.push(min, max);
+    
+    
+    svodkaSelection.sort(function(a, b) {
+    return d3.ascending(a.amount, b.amount)});;
+    
+    var yScale = d3.scale.ordinal()
+    .domain(d3.range(svodkaSelection.length))
+        .rangeRoundBands([height / 2, 0], .2);
+		var xScale = d3.scale.linear().domain([0,
+                      d3.max(svodkaSelection, function(d) { return d.amount; })]).range([0, width / 2]);
+    
+    var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+    var yAxis = d3.svg.axis().scale(yScale).orient("left")
+            .tickFormat(function(d) {
+            var values = svodkaSelection;
+            return values[d].title;
+              });
+    var minMaxScale = d3.scale.ordinal()
+                        .domain(d3.range(10))
+                        .rangeRoundBands([height / 2, 0], .2);
+    var minMaxAxis = d3.svg.axis()
+                      .scale(minMaxScale).orient("right")
+                      //.tickValues([0, 9])
+                      .tickFormat(function(d) {
+                        var values = {"0": "Минимум", "1": "1", "2": "2", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9": "Максимум"}
+                        return values[d];
+                          });
+
+        
+    svodka.selectAll("rect").data(svodkaSelection).enter().append("rect").attr({
+    x: 180,
+    y: function(d, i) {return yScale(i)},
+    width: function(d) {return xScale(d.amount)},
+    height: yScale.rangeBand(),
+    fill: function(d) { if ((d.max) || (d.min)) { return 'rgb(227,26,28)'; } else { return "rgb(253,141,60)" } },
+    title: function(d) { return d.amount; }
+    })
+    svodka.selectAll("text").data(svodkaSelection).enter().append("text")
+    .text(function(d) {return d.amount})
+    .attr({
+        x: function(d) {return xScale(d.amount) + 110; },
+        y: function(d, i) {return yScale(i) + yScale.rangeBand() / 2 + 5},
+        fill: "white" 
+        });
+
+    svodka.append("g").attr("class", "y axis").attr("transform", "translate(180, 0)").call(yAxis);
+    svodka.append("g").attr("class", "x axis").attr("transform", "translate(180, 300)").call(xAxis); 
+    
+    svodka.append("g").attr("class", "minMax axis").attr("transform", "translate(650, 0)").call(minMaxAxis); 
+    
+/////////////////////////
+
     color.domain([d3.min(selection, function(d) { return d.amount }),
                   d3.max(selection, function(d) { return d.amount })]);
     
@@ -74,8 +184,23 @@ d3.json("data/rajony.geojson", function(karta) {
                                     return "black"
                                     }
                                   })
-      .attr("title", function(d) { return d.properties.rajon + " район, " + formatter(d.properties.amount) });
-      
+              .on("mouseover", function(d) {
+                    var xPos = d3.event.pageX + "px";
+                    var yPos = d3.event.pageY + "px";
+                    d3.select("#tooltip")
+                      .style("left", xPos)
+                      .style("top", yPos)
+                      .classed("hidden", false);
+                    d3.select("#rajon")
+                      .text(d.properties.rajon + " район" );
+                    
+                      d3.select("#amount")
+                      .text("Зарплата: " + d.properties.amount + " тыс. руб.");
+            })
+                    .on("mouseout", function(d) {
+                      d3.select("#tooltip")
+                        .classed("hidden", true)
+                      });
       // Легенда
       var legend = svg_map.append("g")
             .attr("id", "legend")
@@ -101,60 +226,145 @@ d3.json("data/rajony.geojson", function(karta) {
                 y: function(d, i) { return (i * 20) + 12; }
             });
       
-        slider.on("slide", function(evt, value) {
-        month = Math.round(value / stepper);
-        selection = data.filter(selectData)
-        
-        color.domain([d3.min(selection, function(d) { return d.amount }),
-                  d3.max(selection, function(d) { return d.amount })]);
-        
-        for (var i = 0; i < selection.length; i++ ) {
-          for (var j = 0; j < karta.features.length; j++) {
-            if (selection[i].rajon == karta.features[j].properties.rajon) {
-              karta.features[j].properties.amount = selection[i].amount;
-              break;
-            }
-          }
-        };
-    
-    
-        svg_map.selectAll("path")
-          .data(karta.features)
-          .transition()
-          .duration(500)
-          .attr("fill", function(d) { if (d.properties.amount) {
-                                        return color(d.properties.amount);
-                                      } else {
-                                        return "black"
-                                        }
-                                      })
-          .attr("title", function(d) { return d.properties.rajon + " район, " + formatter(d.properties.amount) });
-          
-          cities.transition()
-          .duration(500)
-          .attr("fill", function(d) { 
-                      var colorCircle;
-                                for (var q = 0; q < selection.length; q++) {
-                                  if (d.city == selection[q].rajon) {
-                                      colorCircle = color(selection[q].amount);
-                                      d.amount = selection[q].amount;
-                                    } else {
-                                      continue;
-                                  };
-                                };
-                                return colorCircle;
-                                })               
-                       .attr("title", function(d) { return d.city + ", " + formatter(d.amount) });
-          
-          legend.selectAll("text")
-            .data(color.range())
-            .transition()
-            .duration(500)
-            .text(function(d) { return "<" + " " + formatter(d3.max(color.invertExtent(d), function(d) { return d; })); });
+    slider.on("slide", function(evt, value) {
+
+	month = Math.round(value / stepper);
+                selection = data.filter(selectData).sort(function(a, b) { 
+        return d3.ascending(a.amount, b.amount)})
+
+            svodkaSelection = selection.filter(selectSvodka)
+            var min = selection[0], max = selection[selection.length - 1];
+            max.max = "max";
+            min.min = "min";
+            svodkaSelection.push(min, max);
+            svodkaSelection.sort(function(a, b) {
+            return d3.ascending(a.amount, b.amount)});;
+            
+            yScale.domain(d3.range(svodkaSelection.length));
+            
+          xScale.domain([0, d3.max(svodkaSelection, function(d) { return d.amount; })]);
+
+            svodka.selectAll("rect").data(svodkaSelection).transition().duration(500).attr({
+            x: 180,
+            y: function(d, i) {return yScale(i)},
+            width: function(d) {return xScale(d.amount)},
+            height: yScale.rangeBand(),
+            fill: function(d) { if ((d.max) || (d.min)) { return 'rgb(227,26,28)'; } else { return 'rgb(253,141,60)' } },
+            title: function(d) { return d.amount; }
+            });
+            svodka.selectAll("text").data(svodkaSelection).transition().duration(500)
+            .text(function(d) {return d.amount})
+            .attr({
+                x: function(d) {return xScale(d.amount) + 110; },
+                y: function(d, i) {return yScale(i) + yScale.rangeBand() / 2 + 5},
+                fill: "white" 
+                });
+
+            svodka.select(".y.axis").transition().duration(500).call(yAxis);
+            svodka.select(".x.axis").transition().duration(500).call(xAxis); 
+              
+            color.domain([d3.min(selection, function(d) { return d.amount }),
+                          d3.max(selection, function(d) { return d.amount })]);
+                
+                for (var i = 0; i < selection.length; i++ ) {
+                  for (var j = 0; j < karta.features.length; j++) {
+                    if (selection[i].rajon == karta.features[j].properties.rajon) {
+                      karta.features[j].properties.amount = selection[i].amount;
+                      break;
+                    }
+                  }
+                };
+
+                svg_map.selectAll("path")
+                  .data(karta.features)
+                  .transition()
+                  .duration(500)
+                  .attr("fill", function(d) { if (d.properties.amount) {
+                                                return color(d.properties.amount);
+                                              } else {
+                                                return "black"
+                                                }
+                                              });
+            
+            svg_map.selectAll("path")
+                  .data(karta.features)
+              .on("mouseover", function(d) {
+                            var xPos = d3.event.pageX + "px";
+                            var yPos = d3.event.pageY + "px";
+                            d3.select("#tooltip")
+                              .style("left", xPos)
+                              .style("top", yPos)
+                              .classed("hidden", false);
+                            d3.select("#rajon")
+                              .text(d.properties.rajon + " район" );
+                            
+                              d3.select("#amount")
+                              .text("Зарплата: " + d.properties.amount + " тыс. руб.");
+                    })
+                            .on("mouseout", function(d) {
+                              d3.select("#tooltip")
+                                .classed("hidden", true)
+                              });
+                  
+                  cities.transition()
+                  .duration(500)
+                  .attr("fill", function(d) { 
+                              var colorCircle;
+                                        for (var q = 0; q < selection.length; q++) {
+                                          if (d.city == selection[q].rajon) {
+                                              colorCircle = color(selection[q].amount);
+                                              d.amount = selection[q].amount;
+                                            } else {
+                                              continue;
+                                          };
+                                        };
+                                        return colorCircle;
+                                        });
+                                               
+                cities.on("mouseover", function(d) {
+                            var xPos = d3.event.pageX + "px";
+                            var yPos = d3.event.pageY + "px";
+                            d3.select("#tooltip")
+                              .style("left", xPos)
+                              .style("top", yPos)
+                              .classed("hidden", false);
+                            d3.select("#rajon")
+                              .text(d.city);
+                            
+                              d3.select("#amount") 
+                              .text("Зарплата: " + d.amount + " тыс. руб.");
+                    })
+                            .on("mouseout", function(d) {
+                              d3.select("#tooltip")
+                                .classed("hidden", true)
+                              });
+                  
+                  legend.selectAll("text")
+                    .data(color.range())
+                    .transition()
+                    .duration(500)
+                    .text(function(d) { return "<" + " " + formatter(d3.max(color.invertExtent(d), function(d) { return d; })); });
+            
+                   var tableSelection = groups.filter(function(d) { if ((d.period == month) && (d.subgroup != "Всего")) { return d; }; } );
+            
+
+            tableSelected = tableSelection.sort(function(a, b) { return d3.descending(parseInt(a['amount']), parseInt(b['amount'])); }).slice(0, 10);
+	    
+	    var newTableSelection = [];
+	    for (var i = 0; i < tableSelected.length; i++) {
+		newTableSelection.push([tableSelected[i].subgroup, tableSelected[i].amount]);
+	    };
+	    tr.data(tableSelected);
+	    tr.selectAll("td").data(function(d) { var alist = []; alist.push(d.subgroup.toUpperCase(), formatter2(d.amount)); return alist })
+		    .transition()
+		    .duration(500)
+		    .text(function(d) { return d; });
+
+	    setTableMonth(month);
   
   });
   
-  var cities = svg_map.selectAll("circle")
+		var cities = svg_map.selectAll("circle")
                           .data(goroda);
                     
                     
@@ -184,8 +394,32 @@ d3.json("data/rajony.geojson", function(karta) {
                                   };
                                 };
                                 return colorCircle;
-                                })               
-                       .attr("title", function(d) { return d.city + ", " + formatter(d.amount) });
+                                });
+        cities.on("mouseover", function(d) {
+                    var xPos = d3.event.pageX + "px";
+                    var yPos = d3.event.pageY + "px";
+                    d3.select("#tooltip")
+                      .style("left", xPos)
+                      .style("top", yPos)
+                      .classed("hidden", false);
+                    d3.select("#rajon")
+                      .text(d.city );
+                    
+                      d3.select("#amount")
+                      .text("Зарплата: " + d.amount + " тыс. руб.");
+            })
+                    .on("mouseout", function(d) {
+                      d3.select("#tooltip")
+                        .classed("hidden", true)
+                      });
+              
+              
+
+    
+    
+    });
 });
   });
 });
+
+
